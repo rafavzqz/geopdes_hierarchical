a8f8c5816e26021b78245f2d7f59c3784dd11613
vazquez <vazquez@imati.cnr.it>
2015-07-17 17:17:10 +0200

    Removed path file

--- hierarchical/add_my_paths.m
+++
@@ -1,3 +0,0 @@
-addpath(genpath('/home/eduardo/Escritorio/IsogeometricAnalysis/programs/GeoPDEs'));
-addpath(genpath('/home/eduardo/Escritorio/IsogeometricAnalysis/GeoPDEs/nurbs-new'));
-addpath(genpath('/home/eduardo/Escritorio/IsogeometricAnalysis/programs/adaptive igm/aigm/hierarchical'));
