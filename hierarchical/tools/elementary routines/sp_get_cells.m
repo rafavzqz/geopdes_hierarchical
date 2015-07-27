b76fe5237213c5db784e48bd07b447ccd329db57
vazquez <vazquez@imati.cnr.it>
2015-07-27 12:51:45 +0200

    Added output for each single function

--- hierarchical/tools/elementary routines/sp_get_cells.m
+++ hierarchical/tools/elementary routines/sp_get_cells.m
@@ -25,29 +25,29 @@
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function cell_indices = sp_get_cells (space, msh, fun_indices)
+function [cell_indices, indices_per_function] = sp_get_cells (space, msh, fun_indices)
 
 space = sp_precompute_param (space, msh, 'connectivity', true);
+
 conn_indices = arrayfun (@(x) find (space.connectivity == x), fun_indices, 'UniformOutput', false);
-conn_indices = vertcat (conn_indices{:});
-[~, cell_indices] = ind2sub ([space.nsh_max, msh.nel], conn_indices);
-cell_indices = unique (cell_indices(:));
+[~, indices_per_function] = cellfun (@(x) ind2sub ([space.nsh_max, msh.nel], x), conn_indices, 'UniformOutput', false);
+cell_indices = unique (vertcat (indices_per_function{:}));
 
 % % Old version, to be used in case of unexpected memory problems
 % subindices = cell (msh.ndim, 1);
 % [subindices{:}] = ind2sub ([space.ndof_dir, 1], fun_indices); % The extra one makes it work in any dimension
 % 
-% indices = cell (numel (fun_indices), 1);
+% indices_per_function = cell (numel (fun_indices), 1);
 % for ifun = 1:numel (fun_indices)
 %   cells = cell (msh.ndim, 1);
 %   for idim = 1:msh.ndim
 %     cells_1d{idim} = space.sp_univ(idim).supp{subindices{idim}(ifun)};
 %   end
 %   [cells{:}] = ndgrid (cells_1d{:});
-%   indices{ifun} = sub2ind ([msh.nel_dir, 1], cells{:});
+%   indices_per_function{ifun} = sub2ind ([msh.nel_dir, 1], cells{:});
 % end
 % 
-% indices = cellfun(@(x) x(:), indices, 'UniformOutput', false);
-% cell_indices = unique (vertcat (indices{:}));
+% indices_per_function = cellfun(@(x) x(:), indices_per_function, 'UniformOutput', false);
+% cell_indices = unique (vertcat (indices_per_function{:}));
 
 end
