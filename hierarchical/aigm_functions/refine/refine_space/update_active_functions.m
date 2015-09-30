67f1b4c9be63a8d92c0339ce3d0ed0c99abce365
vazquez <vazquez@imati.cnr.it>
2015-09-30 17:26:33 +0200

    New version, removing step 7, not finished yet

--- hierarchical/aigm_functions/refine/refine_space/update_active_functions.m
+++ hierarchical/aigm_functions/refine/refine_space/update_active_functions.m
@@ -52,55 +52,46 @@ end
 for lev = 1:max_lev
   I{lev} = union (I{lev}, intersect (deactivated{lev}, active{lev}));
   if (~isempty(I{lev}))
-    [uno,indA] = ismember (I{lev}, active{lev});
-    if (~all (uno))
-      disp('ERROR: update_active_functions: Some nonactive functions were selected');
-    end
+    [dummy,indA] = ismember (I{lev}, active{lev});
+%     if (~all (dummy))
+%       disp('ERROR: update_active_functions: Some nonactive functions were selected');
+%     end
 
     coefficients = 1;
     for idim = 1:hmsh.ndim
       coefficients = kron (hspace.Proj{lev,idim}, coefficients);
     end
-    
+
     % Remove the corresponding functions from the active functions of level lev
     active{lev}(indA) = [];
     w = W{lev}(indA);
     W{lev}(indA) = [];
     deactivated{lev} = union (deactivated{lev}, I{lev});
     deactivated{lev} = deactivated{lev}(:);
-    
-    % Vectorizar lo que se pueda en el siguiente loop, en particular
+
+    % XXXX Vectorizar lo que se pueda en el siguiente loop, en particular
     % para hacer un solo llamado a sp_get_cells
+    [ii,jj] = find (coefficients(:,I{lev}));
+    children = arrayfun (@(x) ii(jj==x), 1:numel(I{lev}), 'UniformOutput', false);
+
+% Add the children to the active functions, and compute the coefficients for the partition of unity
     for ifun = 1:numel (I{lev})
-      ind = I{lev}(ifun,:);
-      c = coefficients(:,ind);
-      II = find(c);
-      c = c(II);
-      c = full(c);
-      Ichildren = II;
-      % Update deactivated{lev+1}: Computation of functions to be
-      % added to deactivated{lev+1}
-      Ichildren_nonactive = setdiff(Ichildren,active{lev+1},'rows');
-      if ~isempty(Ichildren_nonactive)
-        II = setdiff(Ichildren_nonactive,deactivated{lev+1},'rows');
-        if ~isempty(II)
-          nfun = size(II,1);
-          flag = zeros(1,nfun);
-          [dummy, cells_per_fun] = sp_get_cells(hspace.space_of_level(lev+1), hmsh.mesh_of_level(lev+1), II);
-          for ii = 1: nfun
-            flag(ii) = isempty(intersect(cells_per_fun{ii}, hmsh.active{lev+1},'rows'));
-          end
-          II = II(flag==1,:);
-          deactivated{lev+1} = vertcat(deactivated{lev+1}, II);
-        end
-        nchildren_nonactive = size(Ichildren_nonactive,1);
-        active{lev+1} = vertcat(active{lev+1}, Ichildren_nonactive);
-        W{lev+1} = vertcat(W{lev+1}, zeros(nchildren_nonactive,1));
+      Ichildren = children{ifun};
+      c = coefficients(Ichildren, I{lev}(ifun));
+
+      Ichildren_nonactive = setdiff (Ichildren, active{lev+1});
+      if (~isempty (Ichildren_nonactive))
+        nchildren_nonactive = numel (Ichildren_nonactive);
+        active{lev+1} = vertcat (active{lev+1}, Ichildren_nonactive);
+        W{lev+1} = vertcat (W{lev+1}, zeros(nchildren_nonactive,1));
       end
-      [unos, indices] = ismember(Ichildren, active{lev+1}, 'rows');
+      [dummy, indices] = ismember (Ichildren, active{lev+1});
+%       if (~all (dummy))
+%         disp('ERROR: update_active_functions: Some nonactive functions were selected');
+%       end
       W{lev+1}(indices) = W{lev+1}(indices) + w(ifun)*c;
-    end % for i = 1:numel(I{lev})
-  end
+    end % for ifun = 1:numel(I{lev})
+  end %if
   
 % For the classical hierarchical space, we activate functions of level lev+1, 
 %  that are not children of any removed function of level lev
@@ -126,14 +117,13 @@ for lev = 1:max_lev
 end % for lev
 
 
-
 % Update all the fields in hspace
 if (~isempty(active{hspace.nlevels + 1}))
   hspace.nlevels = hspace.nlevels + 1;
 end
 
-hspace.active = active;
-hspace.deactivated = deactivated;
+hspace.active = active(1:hspace.nlevels);
+hspace.deactivated = deactivated(1:hspace.nlevels);
 hspace.coeff = cell2mat(W);
 hspace.ndof_per_level = cellfun (@numel, hspace.active);
 hspace.ndof = sum(hspace.ndof_per_level);
