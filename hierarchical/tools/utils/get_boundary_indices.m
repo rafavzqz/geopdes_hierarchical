40ef6a2f97b513da3e8197f7a8034cdae72987f9
vazquez <vazquez@imati.cnr.it>
2015-10-02 12:17:38 +0200

    Added help

--- hierarchical/tools/utils/get_boundary_indices.m
+++ hierarchical/tools/utils/get_boundary_indices.m
@@ -1,3 +1,34 @@
+% GET_BOUNDARY_INDICES: given a set of indices (elements or dofs) in the
+%  whole domain, collect those which belong to a given boundary.
+%
+%   bnd_indices = get_boundary_indices (iside, size_dir, indices)
+%
+% INPUT:
+%
+%   iside:    number of the side where to compute the indices
+%   size_dir: the size in each direction (usually given by ndof_dir or nel_dir)
+%   indices:  set of indices in the whole domain
+%
+% OUTPUT:
+%
+%   bnd_indices: set of indices which belong to the boundary, with the
+%     local numbering of the boundary.
+%
+% Copyright (C) 2015 Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function bnd_indices = get_boundary_indices (iside, size_dir, indices)
 
   ndim = numel (size_dir);
