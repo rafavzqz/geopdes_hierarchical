917bf0e1b7b139f84fe4d9af380bf0783f9cf017
vazquez <vazquez@imati.cnr.it>
2016-04-05 12:26:18 +0200

    Remove empty level

--- hierarchical/aigm_functions/@hierarchical_mesh/hmsh_coarsen.m
+++ hierarchical/aigm_functions/@hierarchical_mesh/hmsh_coarsen.m
@@ -58,6 +58,16 @@ else
   hmsh.boundary = [];
 end
 
+% Remove last level if empty (this is not necessary)
+if (isempty (hmsh.active{end}))
+  hmsh.nlevels = hmsh.nlevels - 1;
+  hmsh.active = hmsh.active(1:hmsh.nlevels);
+  hmsh.deactivated = hmsh.deactivated(1:hmsh.nlevels);
+  hmsh.nel_per_level = hmsh.nel_per_level(1:hmsh.nlevels);
+  hmsh.mesh_of_level = hmsh.mesh_of_level(1:hmsh.nlevels);
+  hmsh.msh_lev = hmsh.msh_lev(1:hmsh.nlevels);
+end
+
 end
 
 
