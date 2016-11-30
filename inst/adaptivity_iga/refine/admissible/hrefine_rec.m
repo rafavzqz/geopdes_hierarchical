a408a2cd8e7ac67db1329ee068c3f8aae747b8a5
cesare bracco <cesare.bracco@unifi.it>
2016-11-30 11:57:27 +0100

    Added functions to do admissible refinement

---
+++ inst/adaptivity_iga/refine/admissible/hrefine_rec.m
@@ -0,0 +1,23 @@
+function [new_marked]=hrefine_rec(hmsh, hspace, Q_ind, lev_Q, m)
+%REFINE_RECURSIVE algorithm
+%hmsh: hierarchical mesh to be refined
+%hspace: corresponding hierarchical space to be refined
+%Q_ind: indices of the cell(s) to be refined
+%lev_Q: level of the cell(s) Q_ind
+%m: class of admissibility
+
+new_marked=cell(1,hmsh.nlevels);
+lev_i=lev_Q-m+1;
+neighbours=neigh( hmsh,hspace, Q_ind, lev_Q, m);
+if numel(neighbours)>0
+    new_new_marked=hrefine_rec(hmsh, hspace, neighbours, lev_i, m);
+    for k=1:lev_i
+        new_marked{k}=union(new_marked{k}, new_new_marked{k});
+    end
+end
+
+if numel(intersect(Q_ind,hmsh.active{lev_Q}))>0
+    new_marked{lev_Q}=union(new_marked{lev_Q}, Q_ind);
+end
+
+end
---
+++ inst/adaptivity_iga/refine/admissible/neigh.m
@@ -0,0 +1,19 @@
+function [ list_of_cells ] = neigh(hmsh, hspace, Q_ind, lev_Q, m)
+%NEIGH computes the neighborhood of the cell(s) Q_ind of level lev_Q belonging to
+%the hierarchical mesh hmsh (admissible of class m) associated to the space hspace,
+%according to definition in of [A. Buffa, C. Giannelli, 
+%Adaptive isogeometric methods with hierarchical splines: Error estimator and convergence, 2016]
+
+list_of_cells=[];
+lev_s=lev_Q-m+2;
+if lev_s<=1
+    list_of_cells=[];
+else
+    all_el_lev=1:hmsh.mesh_of_level(lev_s).nel;
+    inactive_el=setdiff(all_el_lev,hmsh.active{lev_s});
+    el=intersect(inactive_el,support_ext(hmsh,hspace,Q_ind,lev_Q, lev_s));
+    anc=get_ancestors(hmsh,el,lev_s,lev_s-1);
+    list_of_cells=union(list_of_cells, intersect(anc,hmsh.active{lev_s-1}));
+end
+
+end
