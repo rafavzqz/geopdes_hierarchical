7539a2ba1efa9c7add707fc89dbcc5055a8a664c
vazquez <vazquez@imati.cnr.it>
2017-01-23 17:27:43 +0100

    Added the exact solver, for curiosity

--- inst/bpx_functions/prec_bpx_new.m
+++ inst/bpx_functions/prec_bpx_new.m
@@ -34,6 +34,22 @@ elseif (iopt == 4) % Richardson
     yy_1 = coeff * xx_1;
     yy = yy + bpx(ii).Qi*yy_1;
   end
+  
+elseif (iopt == 5) % Exact solver everywhere, and Jacobi at the finest
+  for ii = 2:n_level
+    xx_1 = bpx(ii).Qi'*xx;
+
+    A = bpx(ii).Ai(bpx(ii).new_dofs,bpx(ii).new_dofs);
+    yy_1 = A \ xx_1;
+    yy = yy + bpx(ii).Qi*yy_1;
+  end
+%   for ii = n_level
+%     xx_1 = bpx(ii).Qi'*xx;
+% 
+%     A = bpx(ii).Ai(bpx(ii).new_dofs,bpx(ii).new_dofs);
+%     yy_1 = spdiags (spdiags(A,0), 0, size(A,1), size(A,2)) \ xx_1;
+%     yy = yy + bpx(ii).Qi*yy_1;
+%   end
 end
 clear yy_1;
 
