6b78ea425aefdfff52341f72685cae7216a96f23
vazquez <vazquez@imati.cnr.it>
2018-02-09 14:35:41 +0100

    New functions

---
+++ inst/hierarchical_classes/@hierarchical_mesh_mp/hmsh_get_descendants.m
@@ -0,0 +1,50 @@
+function descendants = hmsh_get_descendants (hmsh, Q_ind, lev_Q, lev)
+
+% HMSH_GET_DESCENDANTS: compute the descendants of a given list of elements of a hierarchical mesh
+%
+%   descendants = hmsh_get_descendants (hmsh, Q_ind, lev_Q, lev)
+%
+% INPUT:
+%
+%   hmsh:   object representing the hierarchical mesh (see hierarchical_mesh)
+%   Q_ind:  indices of the input elements, all of the same level
+%   Q_lev:  level of the elements in Q_ind
+%   lev:    level for which to compute the ancestors (lev_Q < lev)
+%
+% OUTPUT:
+%
+%   descendants: indices of the descendants of level lev
+%           
+% Copyright (C) 2017, 2018 Cesare Bracco, Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if (lev_Q < 1 || lev_Q > hmsh.nlevels-1)
+  error ('The level should be between 1 and the number of levels of the mesh minus one')
+end
+
+if (lev <= lev_Q)
+  error('lev<= lev_Q: use get_parent')
+end
+
+if (lev > hmsh.nlevels)
+  error ('The level of the descendants is higher than the finest level of the mesh')
+end
+  
+if (lev == lev_Q+1)
+  descendants = hmsh_get_children (hmsh, lev_Q, Q_ind);
+else
+  descendants = hmsh_get_descendants (hmsh, hmsh_get_children(hmsh, lev_Q, Q_ind), lev_Q+1, lev);
+end
+end
