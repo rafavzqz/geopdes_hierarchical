97a7491bd95193f3ee7ffd328f94467b564330b9
vazquez <vazquez@imati.cnr.it>
2018-02-12 11:18:29 +0100

    Working on admissible coarsening

--- inst/adaptivity_iga/refine/admissible/support_extension.m
+++ inst/adaptivity_iga/refine/admissible/support_extension.m
@@ -17,7 +17,7 @@ function [list_of_cells] = support_extension (hmsh, hspace, Q_ind, lev_Q, lev_s)
 %
 %   list_of_cells: indices, in the tensor product space, of the elements in 
 %                   the support extension
-%           
+%
 % Copyright (C) 2017, 2018 Cesare Bracco, Rafael Vazquez
 %
 %    This program is free software: you can redistribute it and/or modify
@@ -33,12 +33,15 @@ function [list_of_cells] = support_extension (hmsh, hspace, Q_ind, lev_Q, lev_s)
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-if (lev_Q == lev_s)
+if (lev_Q == lev_s || nargin == 4)
     funs = sp_get_basis_functions (hspace.space_of_level(lev_Q), hmsh.mesh_of_level(lev_Q), Q_ind);
     list_of_cells = sp_get_cells (hspace.space_of_level(lev_Q), hmsh.mesh_of_level(lev_Q), funs);
-else
+elseif (lev_Q > lev_s)
     ancestors = hmsh_get_ancestors (hmsh, Q_ind, lev_Q, lev_s);
     list_of_cells = support_extension (hmsh, hspace, ancestors, lev_s, lev_s);
+elseif (lev_Q < lev_s)
+    descendants = hmsh_get_descendants (hmsh, Q_ind, lev_Q, lev_s);
+    list_of_cells = support_extension (hmsh, hspace, descendants, lev_s, lev_s);
 end
 
 end
