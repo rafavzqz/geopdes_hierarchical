a4783d6740f0156e537c19028c8b44c4f8a8fd7a
vazquez <vazquez@imati.cnr.it>
2018-04-26 17:49:12 +0200

    Changes after algorithm discussion

--- inst/adaptivity_iga/refine/admissible/get_neighborhood.m
+++ inst/adaptivity_iga/refine/admissible/get_neighborhood.m
@@ -39,16 +39,15 @@ function [ list_of_cells ] = get_neighborhood (hmsh, hspace, Q_ind, lev_Q, m)
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-list_of_cells = [];
 lev_s = lev_Q-m+2;
 if lev_s <= 1
     list_of_cells = [];
 else
     all_el_lev = 1:hmsh.mesh_of_level(lev_s).nel;
-    inactive_el = setdiff (all_el_lev, hmsh.active{lev_s});
+    inactive_el = setdiff (all_el_lev, union (hmsh.active{lev_s}, hmsh.deactivated{lev_s}));
     el = intersect (inactive_el, support_extension(hmsh,hspace,Q_ind,lev_Q, lev_s));
-    ancestors = hmsh_get_ancestors (hmsh, el, lev_s, lev_s-1);
-    list_of_cells = union (list_of_cells, intersect(ancestors,hmsh.active{lev_s-1}));
+    ancestors = hmsh_get_parent (hmsh, lev_s, el);
+    list_of_cells = intersect (ancestors, hmsh.active{lev_s-1});
 end
 
 end
