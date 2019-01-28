ba9d1b6487f73f0d8bc4e045f4193d6b1585f855
ochanon <ondine.chanon@gmail.com>
2019-01-28 12:04:35 +0100

    cleaned and tested build hmsh hspace from gismo

--- inst/examples/ex_adaptivity_laplace_2d_gismo.m
+++ inst/examples/ex_adaptivity_laplace_2d_gismo.m
@@ -2,8 +2,9 @@
 clear problem_data  
 % Physical domain, defined as NURBS map given in a text file
 problem_data.geo_name = 'geo_rectangle.txt';
-filename = '/Users/ondine/Documents/gismo/filedata/domain2d/rectangleTHB.xml';
-problem_data_gismo.geo_name = gsTHBSpline(filename);
+% Change path if needed!
+filename = '../../../geopdes/geopdes/inst/examples/geometry_files/gismo_rectangleTHB.xml';
+problem_data_gismo.geo_name = gsTHBSpline(filename, 2);
 
 % Type of boundary conditions for each side of the domain
 problem_data.nmnn_sides   = [];
@@ -37,7 +38,7 @@ problem_data_gismo.graduex = problem_data.graduex;
 clear method_data
 method_data.degree      = [2, 2];            % Degree of the splines
 method_data.regularity  = [1, 1];            % Regularity of the splines
-method_data.nsub_coarse = [4, 4];            % Number of subdivisions of the coarsest mesh, with respect to the mesh in geometry
+method_data.nsub_coarse = [1 1];            % Number of subdivisions of the coarsest mesh, with respect to the mesh in geometry
 method_data.nsub_refine = [2, 2];            % Number of subdivisions for each refinement
 method_data.nquad       = [3, 3];            % Points for the Gaussian quadrature rule
 method_data.space_type  = 'standard';        % 'simplified' (only children functions) or 'standard' (full basis)
@@ -61,8 +62,10 @@ plot_data.plot_hmesh = false;
 plot_data.plot_discrete_sol = false;
 plot_data.print_info = true;
 
-[geometry, hmsh, hspace, u, solution_data] = adaptivity_laplace (problem_data, method_data, adaptivity_data, plot_data);
-[geometry_gismo, hmsh_gismo, hspace_gismo, u_gismo, solution_data_gismo] = adaptivity_laplace (problem_data_gismo, method_data, adaptivity_data, plot_data);
+[geometry, hmsh, hspace, u, solution_data] = ...
+    adaptivity_laplace (problem_data, method_data, adaptivity_data, plot_data);
+[geometry_gismo, hmsh_gismo, hspace_gismo, u_gismo, solution_data_gismo] = ...
+    adaptivity_laplace (problem_data_gismo, method_data, adaptivity_data, plot_data);
 
 % EXPORT VTK FILE
 npts = [51 51];
@@ -73,20 +76,16 @@ sp_to_vtk (u_gismo, hspace_gismo, geometry_gismo, npts, output_file_gismo, {'sol
 
 % Plot in Octave/Matlab
 [eu, F] = sp_eval (u, hspace, geometry, npts);
-figure; subplot (1,2,1)
+figure; subplot (1,3,1)
 surf (squeeze(F(1,:,:)), squeeze(F(2,:,:)), eu)
-subplot(1,2,2)
-surf (squeeze(F(1,:,:)), squeeze(F(2,:,:)), squeeze (problem_data.uex(F(1,:,:), F(2,:,:))));
-
+title('Solution with NURBS geometry')
 [eu_gismo, F_gismo] = sp_eval (u_gismo, hspace_gismo, geometry_gismo, npts);
-figure; subplot (1,2,1)
+subplot (1,3,2)
 surf (squeeze(F_gismo(1,:,:)), squeeze(F_gismo(2,:,:)), eu_gismo)
-subplot(1,2,2)
+title('Solution with G+smo geometry')
+subplot(1,3,3)
 surf (squeeze(F_gismo(1,:,:)), squeeze(F_gismo(2,:,:)), squeeze (problem_data_gismo.uex(F_gismo(1,:,:), F_gismo(2,:,:))));
+title('Exact solution')
 
 assert(length(u) == length(u_gismo))
-[eu_err, F_err] = sp_eval (u-u_gismo, hspace, geometry, npts);
-figure; subplot (1,2,1)
-surf (squeeze(F_err(1,:,:)), squeeze(F_err(2,:,:)), eu_err)
-subplot(1,2,2)
-surf (squeeze(F_err(1,:,:)), squeeze(F_err(2,:,:)), squeeze (problem_data.uex(F_err(1,:,:), F_err(2,:,:))));
+assert(max(max(abs(sp_eval (u-u_gismo, hspace, geometry, npts)))) < 1e-13)
