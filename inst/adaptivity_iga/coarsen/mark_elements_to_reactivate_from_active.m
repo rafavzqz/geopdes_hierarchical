42dbe27297d2f47756008a113d98358226a74d05
vazquez <vazquez@imati.cnr.it>
2019-04-25 16:06:58 +0200

    Admissible coarsening, also for H-admissible meshes.

--- inst/adaptivity_iga/coarsen/mark_elements_to_reactivate_from_active.m
+++ inst/adaptivity_iga/coarsen/mark_elements_to_reactivate_from_active.m
@@ -16,6 +16,7 @@
 %    num         :  number of cells (or elements) to be reactivated
 %
 % Copyright (C) 2016, 2017, 2018 Eduardo M. Garau, Rafael Vazquez
+% Copyright (C) 2018, 2019 Massimo Carraturo, Rafael Vazquez
 %
 %    This program is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
@@ -31,6 +32,23 @@
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 function [deact_marked, num] = mark_elements_to_reactivate_from_active (marked, hmsh, hspace, adaptivity_data)
+
+if (~isfield (adaptivity_data, 'coarsening_flag'))
+  adaptivity_data.coarsening_flag = 'all';
+end
+
+if (~isfield(adaptivity_data,'adm_class') || adaptivity_data.adm_class < 2)
+  adm_class = 0;
+else
+  adm_class = adaptivity_data.adm_class;
+end
+
+if (~isfield(adaptivity_data,'adm_type') || isempty (adaptivity_data.adm_type))
+  adm_type = 'T-admissible';
+else
+  adm_type = adaptivity_data.adm_type;
+end
+
 
 deact_marked = cell (hmsh.nlevels, 1);
 
@@ -56,28 +74,42 @@ for lev = hmsh.nlevels-1:-1:1
       parents = parents(ind2);
       children_per_cell = children_per_cell(:,ind2);
       deact_marked{lev} = parents;
-      marked{lev+1} = children_per_cell;
     else
       error ('Unknown option for coarsening, in adaptivity_data.coarsening_flag')
     end
+    marked{lev+1} = children_per_cell;
     
 % Algorithm to recover admissible meshes
-    if (isfield (adaptivity_data, 'adm') && adaptivity_data.adm > 1)
-      lev_s = lev + adaptivity_data.adm;
+    if (adm_class)
+      lev_s = lev + adm_class;
       if (lev_s > hmsh.nlevels)
         continue
       else
         active_and_deact = union (hmsh.active{lev_s}, hmsh.deactivated{lev_s});
         active_and_deact = setdiff (active_and_deact, marked{lev_s});
-        supp_ext = support_extension (hmsh, hspace, children_per_cell(:), lev+1, lev+1);
-        [~, descendants_of_cell] = hmsh_get_descendants (hmsh, supp_ext, lev+1, lev_s);
         keep_inds = [];
-        for iel = 1:numel(deact_marked{lev})
-          supp_ext_local = support_extension (hmsh, hspace, children_per_cell(:,iel), lev+1, lev+1);
-          [~,ia,~] = intersect (supp_ext, supp_ext_local);
-          if (isempty (intersect (descendants_of_cell(:,ia), active_and_deact)))
-            keep_inds = [keep_inds, iel];
-          end          
+        
+        if (strcmpi (adm_type, 'T-admissible'))
+          supp_ext = support_extension (hmsh, hspace, children_per_cell(:), lev+1, lev+1);
+          [~, descendants_of_cell] = hmsh_get_descendants (hmsh, supp_ext, lev+1, lev_s);
+          for iel = 1:numel(deact_marked{lev})
+            supp_ext_local = support_extension (hmsh, hspace, children_per_cell(:,iel), lev+1, lev+1);
+            [~,ia,~] = intersect (supp_ext, supp_ext_local);
+            if (isempty (intersect (descendants_of_cell(:,ia), active_and_deact)))
+              keep_inds = [keep_inds, iel];
+            end          
+          end
+          
+        elseif (strcmpi (adm_type, 'H-admissible'))
+          supp_ext = support_extension (hmsh, hspace, deact_marked{lev}, lev, lev);
+          [~, descendants_of_cell] = hmsh_get_descendants (hmsh, supp_ext, lev, lev_s);
+          for iel = 1:numel(deact_marked{lev})
+            supp_ext_local = support_extension (hmsh, hspace, deact_marked{lev}(iel), lev, lev);
+            [~,ia,~] = intersect (supp_ext, supp_ext_local);
+            if (isempty (intersect (descendants_of_cell(:,ia), active_and_deact)))
+              keep_inds = [keep_inds, iel];
+            end          
+          end
         end
         deact_marked{lev} = deact_marked{lev}(keep_inds);
       end
