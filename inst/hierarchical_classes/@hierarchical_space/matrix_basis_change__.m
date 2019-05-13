f433397cfa17b353436c117da10533cfe970a165
vazquez <vazquez@imati.cnr.it>
2019-05-13 15:43:11 +0200

    Working towards ind_coarse without ind_fine (for compatibility reasons)

--- inst/hierarchical_classes/@hierarchical_space/matrix_basis_change__.m
+++ inst/hierarchical_classes/@hierarchical_space/matrix_basis_change__.m
@@ -1,7 +1,7 @@
 % MATRIX_BASIS_CHANGE__: compute the subdivision matrix between two consecutive levels.
 %        This method is intended to remain private.
 %
-% function C = matrix_basis_change__ (hspace, lev, [ind_coarse])
+% function C = matrix_basis_change__ (hspace, lev, [ind_coarse], [ind_fine])
 %
 % Compute the new matrices to represent functions of level "lev-1"
 % as linear combinations of splines (active and inactive) of level "lev"
@@ -11,12 +11,18 @@
 %   hspace: an object of the class hierarchical_space
 %   lev:    the level for which we compute the matrix
 %   ind_coarse: column indices for which to compute the output matrix
+%   ind_fine:   row indices for which to compute the output matrix
 %
 % OUTPUT:
 %
 %   C:    matrix to change basis from level lev-1 to level lev
 %
+% If ind_fine is not given, the output matrix has size ndof(lev) x ndof(lev-1), 
+%  independently of the presence of ind_coarse, for compatibility with previous versions
+% If ind_fine is given, the output matrix has size numel(ind_fine) x numel(ind_coarse)
+%
 % Copyright (C) 2015, 2016 Eduardo M. Garau, Rafael Vazquez
+% Copyright (C) 2018-2019 Luca Coradello
 % Copyright (C) 2017-2019 Rafael Vazquez
 %
 %    This program is free software: you can redistribute it and/or modify
@@ -37,17 +43,18 @@ function C = matrix_basis_change__ (hspace, lev, ind_coarse, ind_fine)
 Proj = hspace.Proj{lev-1};
 if (nargin == 4)
   C = subdivision_matrix_two_levels__ (hspace.space_of_level(lev-1), hspace.space_of_level(lev), Proj, ind_coarse, ind_fine);
-  if (hspace.truncated)
-    indices = union (hspace.active{lev}, hspace.deactivated{lev});
-    [~, loc] = ismember(indices,ind_fine);  
-    C(loc,:) = 0;
-  end
+elseif (nargin == 3)
+  C = subdivision_matrix_two_levels__ (hspace.space_of_level(lev-1), hspace.space_of_level(lev), Proj, ind_coarse);  
 else
   C = subdivision_matrix_two_levels__ (hspace.space_of_level(lev-1), hspace.space_of_level(lev), Proj);
-  if (hspace.truncated)
-    indices = union (hspace.active{lev}, hspace.deactivated{lev});
-    C(indices,:) = 0;
+end
+
+if (hspace.truncated)
+  indices = union (hspace.active{lev}, hspace.deactivated{lev});
+  if (nargin == 4)
+    [~, indices] = ismember(indices,ind_fine);
   end
+  C(indices,:) = 0;
 end
 
 end
--- inst/hierarchical_classes/@hierarchical_space_mp/matrix_basis_change__.m
+++ inst/hierarchical_classes/@hierarchical_space_mp/matrix_basis_change__.m
@@ -1,7 +1,7 @@
 % MATRIX_BASIS_CHANGE__: compute the subdivision matrix between two consecutive levels.
 %        This method is intended to remain private.
 %
-% function C = matrix_basis_change__ (hspace, lev, [ind_coarse])
+% function C = matrix_basis_change__ (hspace, lev, [ind_coarse], [ind_fine])
 %
 % Compute the new matrices to represent functions of level "lev-1"
 % as linear combinations of splines (active and inactive) of level "lev"
@@ -11,12 +11,18 @@
 %   hspace: an object of the class hierarchical_space_mp
 %   lev:    the level for which we compute the matrix
 %   ind_coarse: column indices for which to compute the output matrix
+%   ind_fine:   row indices for which to compute the output matrix
 %
 % OUTPUT:
 %
 %   C:    matrix to change basis from level lev-1 to level lev
 %
+% If ind_fine is not given, the output matrix has size ndof(lev) x ndof(lev-1), 
+%  independently of the presence of ind_coarse, for compatibility with previous versions
+% If ind_fine is given, the output matrix has size numel(ind_fine) x numel(ind_coarse)
+%
 % Copyright (C) 2015, 2016 Eduardo M. Garau, Rafael Vazquez
+% Copyright (C) 2018-2019 Luca Coradello
 % Copyright (C) 2017-2019 Rafael Vazquez
 %
 %    This program is free software: you can redistribute it and/or modify
@@ -40,9 +46,8 @@ sp_coarse = hspace.space_of_level(lev-1);
 sp_fine = hspace.space_of_level(lev);
 Proj = hspace.Proj(lev-1,:);
 
-% C = sparse (sp_fine.ndof, sp_coarse.ndof);
-
 if (nargin == 4)
+  C = sparse (numel(ind_fine), numel(ind_coarse));
   for iptc = 1:npatch
     spc_patch = sp_coarse.sp_patch{iptc};
     spf_patch = sp_fine.sp_patch{iptc};
@@ -52,25 +57,33 @@ if (nargin == 4)
     Cpatch = subdivision_matrix_two_levels__ (spc_patch, spf_patch, Proj{iptc}, local_indices_coarse, local_indices_fine);
  
     C(ind_f, ind_c) = Cpatch;
-
   end
-  if (hspace.truncated)
-    indices = union (hspace.active{lev}, hspace.deactivated{lev});
-    [~, loc] = ismember(indices,ind_fine);  
-    C(loc,:) = 0;
-  end  
+elseif (nargin == 3)
+  C = sparse (sp_fine.ndof, sp_coarse.ndof);
+  for iptc = 1:npatch
+    spc_patch = sp_coarse.sp_patch{iptc};
+    spf_patch = sp_fine.sp_patch{iptc};
+    [~,local_indices,~] = intersect (sp_coarse.gnum{iptc}, ind_coarse);
+    Cpatch = subdivision_matrix_two_levels__ (spc_patch, spf_patch, Proj{iptc}, local_indices);
+    C(sp_fine.gnum{iptc},sp_coarse.gnum{iptc}) = Cpatch;
+  end
+
 else
+  C = sparse (sp_fine.ndof, sp_coarse.ndof);
   for iptc = 1:npatch
     spc_patch = sp_coarse.sp_patch{iptc};
     spf_patch = sp_fine.sp_patch{iptc};
     Cpatch = subdivision_matrix_two_levels__ (spc_patch, spf_patch, Proj{iptc});
     C(sp_fine.gnum{iptc},sp_coarse.gnum{iptc}) = Cpatch;
   end
-  
-  if (hspace.truncated)
-    indices = union (hspace.active{lev}, hspace.deactivated{lev});
-    C(indices,:) = 0;
+end
+
+if (hspace.truncated)
+  indices = union (hspace.active{lev}, hspace.deactivated{lev});
+  if (nargin == 4)
+    [~, indices] = ismember(indices,ind_fine);
   end
+  C(indices,:) = 0;
 end
 
 end
