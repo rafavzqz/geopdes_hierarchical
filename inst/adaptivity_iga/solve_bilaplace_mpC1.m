aa304674ef32cec0c1cebe4553a352e0dd3bc68d
Andrea Farahat <andrea.farahat@ricam.oeaw.ac.at>
2022-06-13 16:06:24 +0200

    Added file to compute the boundary conditions by using the exact solution and gradient + kernel computation for dirichlet boundary vertices in the hierarchical setting

--- inst/adaptivity_iga/solve_bilaplace_mpC1.m
+++ inst/adaptivity_iga/solve_bilaplace_mpC1.m
@@ -77,13 +77,53 @@ u = zeros (hspace.ndof, 1);
 % Apply Dirichlet boundary conditions in strong form, both for the value
 %  and the normal derivative
 % Here we are assuming that all the boundary has Dirichlet conditions
-[u_drchlt, drchlt_dofs] = sp_bilaplacian_drchlt_C1 (hspace, hmsh, drchlt_sides, h, g);
+if (isfield(problem_data, 'graduex') && isfield(problem_data, 'uex'))
+    [u_drchlt, drchlt_dofs, kernel_dofs] = sp_bilaplacian_drchlt_C1_exact (hspace, hmsh, drchlt_sides, uex, graduex);
+else
+    [u_drchlt, drchlt_dofs, kernel_dofs] = sp_bilaplacian_drchlt_C1 (hspace, hmsh, drchlt_sides, h, g);
+end
+
+% Old code
+
+% u(drchlt_dofs) = u_drchlt;
+% 
+% int_dofs = setdiff (1:hspace.ndof, drchlt_dofs);
+% rhs(int_dofs) = rhs(int_dofs) - stiff_mat(int_dofs, drchlt_dofs)*u_drchlt;
+% 
+% % Solve the linear system
+% u(int_dofs) = stiff_mat(int_dofs,int_dofs) \ rhs(int_dofs);
+% 
+% end
+
+% New part
+
 u(drchlt_dofs) = u_drchlt;
 
 int_dofs = setdiff (1:hspace.ndof, drchlt_dofs);
+add_dofs = kernel_dofs.quasi_interior_dofs; % This will contain the "boundary" vertex dofs which have been removed from drchlt_dofs
+
+% We assemble the (pieces of the) stiffness matrix, the rhs (and its correction taking 
+% into account the Dirichlet conditions), and the basis change matrix (we will need it 
+% to go from the basis with kernel vectors obtained when examnining the dirichlet conditions 
+% to the usual basis)
+vertex_dofs = kernel_dofs.all_vertex_dofs;
+% B_change = speye (space.ndof); %basis change matrix
+% B_change(kernel_dofs.all_vertex_dofs,kernel_dofs.quasi_interior_dofs) = kernel_dofs.B_change_local;
+
+stiff_mat(:,add_dofs) = stiff_mat(:,vertex_dofs) * kernel_dofs.B_change_local;
+stiff_mat(add_dofs,:) = kernel_dofs.B_change_local.' * stiff_mat(vertex_dofs,:);
+rhs(add_dofs) = kernel_dofs.B_change_local.' * rhs(vertex_dofs);
+
 rhs(int_dofs) = rhs(int_dofs) - stiff_mat(int_dofs, drchlt_dofs)*u_drchlt;
 
 % Solve the linear system
-u(int_dofs) = stiff_mat(int_dofs,int_dofs) \ rhs(int_dofs);
+u(int_dofs) = stiff_mat(int_dofs, int_dofs) \ rhs(int_dofs);
+
+% Switching to the usual basis using the local matrix for the vertex dofs
+u_old = u(setdiff(kernel_dofs.all_vertex_dofs, add_dofs)); % Coefficients of the vertex functions that were already in the old basis
+u(kernel_dofs.all_vertex_dofs) = kernel_dofs.B_change_local * u(kernel_dofs.quasi_interior_dofs);
+u(setdiff(kernel_dofs.all_vertex_dofs, add_dofs)) = u(setdiff(kernel_dofs.all_vertex_dofs, add_dofs)) + u_old;
+
+% u = B_change * u;
 
 end
