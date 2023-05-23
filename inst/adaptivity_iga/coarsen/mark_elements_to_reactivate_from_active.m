8b96f9d9d35fce7e5237cc0f4d829f4f0b75651e
cesare <cesare.kr@gmail.com>
2023-05-23 12:53:33 +0200

    fixed interaction admissibility-vertex neighbourhood in mark_elements_to_reactivate_from_active

--- inst/adaptivity_iga/coarsen/mark_elements_to_reactivate_from_active.m
+++ inst/adaptivity_iga/coarsen/mark_elements_to_reactivate_from_active.m
@@ -88,32 +88,13 @@ for lev = hmsh.nlevels-1:-1:1
     marked{lev+1} = children_per_cell;
 
     
-% Algorithm to take into account the vertex patch (multipatch C^1 spaces) 
-    for iv=1:numel(hspace.space_of_level(lev).vertices)
-        adj_els=msh_cells_near_vertex (hmsh.mesh_of_level(lev), hspace.space_of_level(lev).vertices(iv));
-        for i_el=1:length(adj_els)
-            patch_of_el=1;
-            while sum(hmsh.mesh_of_level(lev).nel_per_patch(1:patch_of_el))<adj_els(i_el)
-                patch_of_el=patch_of_el+1;
-            end
-            patch_of_el=find(hspace.space_of_level(lev).vertices(iv).patches==patch_of_el); %we need the local (w.r.t. to the vertex) index of the patch
-            if ~ismember(adj_els(i_el),deact_marked{lev}) && ismember(adj_els(i_el),hmsh.deactivated{lev})
-               en=sp_get_vertex_neighbors (hspace.space_of_level(lev), hmsh.mesh_of_level(lev), iv, patch_of_el); 
-               deact_marked{lev}=setdiff(deact_marked{lev},en{1});
-            end
-        end
-    end
-% TODO: children_per_cell should be updated before marked{lev+1} (as above)
-    [~,~,marked{lev+1}] = hmsh_get_children (hmsh, lev, deact_marked{lev});
-    children_per_cell = marked{lev+1};
+
 
     
 % Algorithm to recover admissible meshes
     if (adm_class)
       lev_s = lev + adm_class;
-      if (lev_s > hmsh.nlevels)
-        continue
-      else
+      if (lev_s <= hmsh.nlevels)
         active_and_deact = union (hmsh.active{lev_s}, hmsh.deactivated{lev_s});
         active_and_deact = setdiff (active_and_deact, marked{lev_s});
         keep_inds = [];
@@ -143,6 +124,25 @@ for lev = hmsh.nlevels-1:-1:1
         deact_marked{lev} = deact_marked{lev}(keep_inds);
       end
     end
+    
+    % Algorithm to take into account the vertex patch (multipatch C^1 spaces) 
+    for iv=1:numel(hspace.space_of_level(lev).vertices)
+        adj_els=msh_cells_near_vertex (hmsh.mesh_of_level(lev), hspace.space_of_level(lev).vertices(iv));
+        for i_el=1:length(adj_els)
+            patch_of_el=1;
+            while sum(hmsh.mesh_of_level(lev).nel_per_patch(1:patch_of_el))<adj_els(i_el)
+                patch_of_el=patch_of_el+1;
+            end
+            patch_of_el=find(hspace.space_of_level(lev).vertices(iv).patches==patch_of_el); %we need the local (w.r.t. to the vertex) index of the patch
+            if ~ismember(adj_els(i_el),deact_marked{lev}) && ismember(adj_els(i_el),hmsh.deactivated{lev})
+               en=sp_get_vertex_neighbors (hspace.space_of_level(lev), hmsh.mesh_of_level(lev), iv, patch_of_el); 
+               deact_marked{lev}=setdiff(deact_marked{lev},en{1});
+            end
+        end
+    end
+% TODO: children_per_cell should be updated before marked{lev+1} (as above)
+    [~,~,marked{lev+1}] = hmsh_get_children (hmsh, lev, deact_marked{lev});
+    children_per_cell = marked{lev+1};
     
   end
 end
