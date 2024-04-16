1a0b46c188f55dc9e98c0d145b3f7e7b145dd537
Rafael Vazquez <vazquez@imati.cnr.it>
2024-04-16 15:22:50 +0200

    New version without using finest level

--- inst/hierarchical_classes/@hierarchical_space_mp_C1/sp_eval_phys.m
+++ inst/hierarchical_classes/@hierarchical_space_mp_C1/sp_eval_phys.m
@@ -37,7 +37,7 @@
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function eu = sp_eval_phys (u, hspace, hmsh, geometry, pts, varargin)
+function eu = sp_eval_phys (u, hspace, hmsh, geometry, pts, patch_list, varargin)
 
   if (numel (u) ~= hspace.ndof && numel(u) ~= hmsh.rdim*hspace.ndof)
     error ('The number of degrees of freedom of the vector and the space do not match')
@@ -49,14 +49,85 @@ function eu = sp_eval_phys (u, hspace, hmsh, geometry, pts, varargin)
     error ('Only implemented for NURBS geometries')
   end
 
-  Csub = hspace_subdivision_matrix (hspace, hmsh, 'full');
-  Csub = Csub{end};
-  if (numel(u) == hspace.ndof)
-    eu = sp_eval_phys (Csub * u, hspace.space_of_level(end), geometry, pts, varargin{:});
-  else
-    Csub = repmat({Csub}, hmsh.rdim, 1);
-    eu = sp_eval_phys (blkdiag(Csub{:}) * u, hspace.space_of_level(end), geometry, pts, varargin{:});
-  end
+  npts = size (pts, 2);
+  ndim = hmsh.ndim;
+  rdim = hmsh.rdim;
+  npatch = numel (geometry);
+  pts_param = zeros (ndim, npts); flag = false (npts, 1);
   
+% This is necessary because the NURBS toolbox works in the 3D space
+  if (size(pts, 1) < 3)
+    pts(end+1:3,:) = 0;
+  end
+
+  if (nargin < 6 || isempty (patch_list))
+    patch_list = zeros (1, npts);
+    for ipnt = 1:npts
+      for iptc = 1:npatch
+        [pts_param(:,ipnt), flag(ipnt)] = nrbinverse (geometry(iptc).nurbs, pts(:,ipnt));
+        if (flag(ipnt))
+          patch_list(ipnt) = iptc;
+          break
+        end
+      end
+    end
+  else
+    for ipnt = 1:npts
+      iptc = patch_list(ipnt);
+      [pts_param(:,ipnt), flag(ipnt)] = nrbinverse (geometry(iptc).nurbs, pts(:,ipnt));
+    end
+  end
+
+  pts_on_patch = cell (1, npatch);
+  msh_aux = struct ('ndim', ndim, 'rdim', rdim, 'boundary', []);
+  res = cell (npts, 1);
+  for ipnt = 1:npts
+    iptc = patch_list(ipnt);
+    coords = pts_param(:,ipnt);
+    coords_cell = mat2cell (coords, ones(numel(coords), 1));
+    for ilev = 1:hmsh.nlevels
+      breaks = hmsh.mesh_of_level(ilev).msh_patch{iptc}.breaks;
+      for idim = 1:ndim
+        ind_elem{idim} = findspan (numel(breaks{idim})-2, 0, coords(idim), breaks{idim}) + 1;
+      end
+      elem_on_patch = sub2ind (hmsh.mesh_of_level(ilev).msh_patch{iptc}.nel_dir,ind_elem{:});
+      elem_number = sum(hmsh.mesh_of_level(ilev).nel_per_patch(1:iptc-1)) + elem_on_patch;
+
+      if (ismember (elem_number, hmsh.active{ilev}))
+        Csub = repmat (hspace.Csub(ilev), 1, rdim);
+        Csub = blkdiag (Csub{:});
+        inds = [];
+        ndof_until_lev = sum(hspace.ndof_per_level(1:ilev));
+        for idim = 1:rdim
+          inds = union (inds, (1:ndof_until_lev)+(idim-1)*hspace.ndof);
+        end
+        % u_rows = Csub * u(1:sum(hspace.ndof_per_level(1:ilev)*rdim));
+        u_rows = Csub * u(inds);
+
+        Csub_rows = [];
+        ndof_lev = hspace.space_of_level(ilev).ndof;
+        for icomp = 1:rdim
+          Csub_rows = union (Csub_rows, (icomp-1)*ndof_lev + hspace.Csub_row_indices{ilev});
+        end
+        [Cpatch, Cpatch_cols] = sp_compute_Cpatch_vector (hspace.space_of_level(ilev), iptc, rdim);
+        [~,iCp,iCs] = intersect (Cpatch_cols, Csub_rows);
+        u_ptc_lev = Cpatch(:,iCp) * u_rows(iCs);
+          
+        sp_vec = sp_vector (repmat (hspace.space_of_level(ilev).sp_patch(iptc), rdim, 1), msh_aux);
+        res{ipnt} = sp_eval (u_ptc_lev, sp_vec, geometry(iptc), coords_cell(:)', varargin{:});
+        break
+      end
+    end
+  end
+  eu = cat(2,res{:});
+
+  % Csub = hspace_subdivision_matrix (hspace, hmsh, 'full');
+  % Csub = Csub{end};
+  % if (numel(u) == hspace.ndof)
+  %   eu = sp_eval_phys (Csub * u, hspace.space_of_level(end), geometry, pts, patch_list, varargin{:});
+  % else
+  %   Csub = repmat({Csub}, rdim, 1);
+  %   eu = sp_eval_phys (blkdiag(Csub{:}) * u, hspace.space_of_level(end), geometry, pts, patch_list, varargin{:});
+  % end
 end
   
