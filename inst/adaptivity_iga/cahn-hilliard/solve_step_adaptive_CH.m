6faa9d3618041c4a8f33d2236cd6e3613044a45d
Rafael Vazquez <vazquez@imati.cnr.it>
2024-07-18 19:10:18 +0200

    Added help. Changed function name.

--- inst/adaptivity_iga/cahn-hilliard/solve_step_adaptive_CH.m
+++ inst/adaptivity_iga/cahn-hilliard/solve_step_adaptive_CH.m
@@ -1,7 +1,48 @@
-%--------------------------------------------------------------------------
-% adaptivity in space
-%--------------------------------------------------------------------------
-function [u_n1, udot_n1, hspace, hmsh, est, old_space] = solve_step_adaptive...
+% SOLVE_STEP_ADAPTIVE_CH: performs one step of the generalized alpha method
+%  for the solution of the Cahn-Hilliear equation.
+%  It is called from adaptive_cahn_hilliard or adaptive_cahn_hilliard_mp_C1
+%
+% INPUT:
+%
+%  u_n:          field at the previous time step.
+%  u_dotn:       time derivative at the previous time step.
+%  hspace:       space object (see hierarhical_space or hierarchical_space_mp_C1)
+%  hmsh:         mesh object (see hierarchical_mesh or hierarchical_mesh_mp)
+%  dt:           time step size
+%  a_m:          parameter for the generalized alpha method
+%  a_f:          parameter for the generalized alpha method
+%  gamma:        parameter for the generalized alpha method
+%  Cpen:         penalization parameter for Nitsche's method
+%  problem_data: a structure with data of the problem.
+%  adaptivity_data: a structure with data for adaptivity (in space).
+%  old_space:    space from previous iterations. If not changed, some matrices are not recomputed.
+%  nmnn_sides:   sides where to impose the Neumann condition.
+%
+% OUTPUT:
+%
+%  u_n1:      field at the previous time step.
+%  u_dotn1:   time derivative at the previous time step.
+%  hspace:    space object, it can be refined or coarsened (see hierarhical_space or hierarchical_space_mp_C1)
+%  hmsh:      mesh object, it can be refined or coarsened (see hierarchical_mesh or hierarchical_mesh_mp)
+%  est:       refinement indicator
+%  old_space: space of the previous iteration, updated
+%
+% Copyright (C) 2023 Michele Torre, Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function [u_n1, udot_n1, hspace, hmsh, est, old_space] = solve_step_adaptive_CH...
   (u_n, udot_n, hspace, hmsh, dt, a_m, a_f, gamma, Cpen, ...
    problem_data, adaptivity_data, old_space, nmnn_sides)
 
