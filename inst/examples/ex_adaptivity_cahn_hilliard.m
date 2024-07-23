30741ed46b0b3e0eed32a4ddcf81a34c6526b653
Rafael Vazquez <vazquez@imati.cnr.it>
2024-07-23 19:58:21 +0200

    Some changes to make them work. Unclean

--- inst/examples/ex_adaptivity_cahn_hilliard.m
+++ inst/examples/ex_adaptivity_cahn_hilliard.m
@@ -1,33 +1,46 @@
-nel =1;
-
 % 1) PHYSICAL DATA OF THE PROBLEM
 clear problem_data  
 % Physical domain, defined as NURBS map given in a text file
 problem_data.geo_name = 'geo_square.txt';
 
-% Physical parameters
+% Physical parameters (see solve_cahn_hilliard)
 lambda = 6.15e-4;
+% lambda = (1/(4*sqrt(2)*pi))^2;
 problem_data.lambda = @(x, y) lambda* ones(size(x));
+alpha = 1;
+beta  = 1;
+problem_data.mu  = @(x) 3 * alpha * x.^2 - beta;
+problem_data.dmu = @(x) 6 * alpha * x;
 
-% time  
+% Time  
 problem_data.initial_time = 0;
-problem_data.Time_max = 0.021;    
+problem_data.Time_max = 0.1;    
+
+% Initial conditions
+mean = 0.4;
+var = 0.005;
+ic_fun = @(x, y) mean + (rand(size(x))*2-1)*var;
+ic_fun = @(x, y) 0.1 * cos(2*pi*x) .* cos(2*pi*y);
+
+%ic_fun = load("initial_conditions_nel_64_64_p_2_2.mat");
+%ic_fun = ic_fun.u_0;
+problem_data.fun_u = ic_fun;
+% problem_data.fun_udot = [];
 
 % 2) CHOICE OF THE DISCRETIZATION PARAMETERS
 clear method_data
-p = 2;
-method_data.degree     = [p p];         % Degree of the splines
-method_data.regularity = [p-1 p-1];     % Regularity of the splines
-%method_data.nsub       = [nel nel];     % Number of subdivisions
-method_data.nquad      = [p+1 p+1];     % Points for the Gaussian quadrature rule
+deg = 2;
+method_data.degree     = [deg deg];     % Degree of the splines
+method_data.regularity = [deg-1 deg-1]; % Regularity of the splines
+method_data.nquad      = [deg+1 deg+1]; % Points for the Gaussian quadrature rule
 
 % time integration parameters
 method_data.rho_inf_gen_alpha = 0.5;
 method_data.dt = 1e-3;
 
 % penalty parameters
-method_data.pen_nitsche = 1e4 * lambda; % Nitsche's  penalty constant 
-method_data.pen_projection = 1000;      % penalty value to constrain the flux in L2 projection
+method_data.Cpen_nitsche = 1e4 * lambda; % Nitsche's  penalty constant 
+method_data.Cpen_projection = 1000;      % Parameter of the penalized L2 projection (see initial conditions)
 
 % hierarchical structure
 nel = 1;
@@ -39,9 +52,8 @@ method_data.truncated   = 1;            % 0: False, 1: True
 % ADAPTIVITY PARAMETERS
 clear adaptivity_data
 adaptivity_data.flag = 'elements';
-adaptivity_data.C0_est = 1.0;
 
-adaptivity_data.estimator_type = 'field';
+adaptivity_data.estimator_type = 'field'; % Either 'field' or 'gradient'
 adaptivity_data.mark_param = .2;
 adaptivity_data.mark_param_coarsening = .2;
 adaptivity_data.time_delay = 0.;
@@ -55,34 +67,22 @@ adaptivity_data.tol = 1e-5;
 adaptivity_data.adm_type = 'T-admissible';
 adaptivity_data.adm_class = 2;
 
-% 3) INITIAL CONDITIONS 
-clear initial_conditions
-mean = 0.4;
-var = 0.005;
-ic_fun = @(x, y) mean + (rand(size(x))*2-1)*var;
-ic_fun = @(x, y) 0.1 * cos(2*pi*x) .* cos(2*pi*y);
-
-
-%ic_fun = load("initial_conditions_nel_64_64_p_2_2.mat");
-%ic_fun = ic_fun.u_0;
-initial_conditions.fun_u = ic_fun;
-
-
-% create folder
-folder_name = strcat('chan_hilliard_results_p',num2str(p),'_nel',num2str(nel),'_lambda',num2str(lambda),'_adaptive_',adaptivity_data.estimator_type);
-status = mkdir(folder_name);
-status = rmdir(folder_name);
-status = mkdir(folder_name);
+% Output data
+folder_name = strcat('cahn_hilliard_results_p',num2str(deg),'_nel',num2str(nel),'_lambda',num2str(lambda),'_adaptive_',adaptivity_data.estimator_type);
+file_name = 'Square_Cahn_Hilliard_adaptive';
+status = mkdir (folder_name);
 
 save_info.folder_name = folder_name;
+save_info.file_name = file_name;
 save_info.time_save = linspace(-.000001,problem_data.Time_max,3);
+save_info.vtk_pts = {linspace(0,1,150), linspace(0,1,150)};
 
 % 3) CALL TO THE SOLVER
-[geometry, hmsh, hspace, results] = adaptivity_cahn_hilliard(problem_data, method_data, adaptivity_data, initial_conditions, save_info);
+[geometry, hmsh, hspace, results] = ...
+  adaptivity_cahn_hilliard(problem_data, method_data, adaptivity_data, save_info);
 
 
-%% 4) POST-PROCESSING
+% 4) POST-PROCESSING
 filename = strcat( folder_name,'/filenum_to_time_uniform_mesh.mat');
 time_steps = results.time;
 save(filename, 'time_steps');
-
--- inst/examples/ex_adaptivity_cahn_hilliard_mpC1.m
+++ inst/examples/ex_adaptivity_cahn_hilliard_mpC1.m
@@ -1,30 +1,20 @@
-% clc
-% close all
-% clear all
-
-warning ('off','geopdes:nrbmultipatch')
-
 % 1) PHYSICAL DATA OF THE PROBLEM
 clear problem_data  
-% Physical domain, defined as NURBS map given in a text file
+% Physical domain, defined as multipatch AS-G1 
 problem_data.geo_name = 'curved_3patch_bicubic.txt' ;
 
 % Physical parameters
 lambda = 5e-2;
 problem_data.lambda = @(x, y) lambda* ones(size(x));
-
+alpha = 1;
+beta  = 1;
+problem_data.mu  = @(x) 3 * alpha * x.^2 - beta;
+problem_data.dmu = @(x) 6 * alpha * x;
 
 % Time and time step size
-Time_max = 50;
-dt = 1e-1;
-problem_data.time = 0;
-problem_data.Time_max = Time_max;
-
-
-% penalty parameters
-problem_data.Cpen_nitsche = 1e4 * lambda; % Nitsche's method parameter
-problem_data.Cpen_projection = 1000;      % parameter of the penalized L2 projection (see initial conditions)
-
+problem_data.time = 0; % TODO: REMOVE
+problem_data.initial_time = 0;
+problem_data.Time_max = 1;
 
 % 2) CHOICE OF THE DISCRETIZATION PARAMETERS
 clear method_data
@@ -32,12 +22,16 @@ nel = 3;
 p = 3;
 method_data.degree     = [p p];         % Degree of the splines
 method_data.regularity = [p-2 p-2];     % Regularity of the splines
-%method_data.nsub       = [nel nel];     % Number of subdivisions
 method_data.nquad      = [p+1 p+1];     % Points for the Gaussian quadrature rule
+method_data.interface_regularity = 1;
 
 % time integration parameters
 method_data.rho_inf_gen_alpha = 0.5;
-method_data.dt =dt;
+method_data.dt = 1e-1;
+
+% penalty parameters
+method_data.Cpen_nitsche = 1e4 * lambda; % Nitsche's method parameter
+method_data.Cpen_projection = 1000;      % parameter of the penalized L2 projection (see initial conditions)
 
 % hierarchical structure
 method_data.nsub_coarse = [nel nel];    % Number of subdivisions of the coarsest mesh, with respect to the mesh in geometry
@@ -45,19 +39,14 @@ method_data.nsub_refine = [2 2];        % Number of subdivisions for each refine
 method_data.space_type  = 'standard';   % 'simplified' (only children functions) or 'standard' (full basis)
 method_data.truncated   = 1;            % 0: False, 1: True
 
-
 % ADAPTIVITY PARAMETERS
 clear adaptivity_data
 adaptivity_data.flag = 'elements';
-adaptivity_data.C0_est = 1.0;
-
 
 adaptivity_data.estimator_type = 'field';
 adaptivity_data.mark_param = .1;
 adaptivity_data.mark_param_coarsening = .1;
-adaptivity_data.adm_class = p;
 adaptivity_data.time_delay = 0.;
-
 
 adaptivity_data.mark_strategy = 'MS';
 adaptivity_data.max_level = 3;
@@ -66,18 +55,16 @@ adaptivity_data.num_max_iter = 10;
 adaptivity_data.max_nel = 500;
 adaptivity_data.tol = 1e-5;
 
-
 adaptivity_data.adm_type = 'T-admissible';
+adaptivity_data.adm_class = p;
 
 % 3) INITIAL CONDITIONS 
 clear initial_conditions
-initial_conditions.restart_flag = 0;
+% initial_conditions.restart_flag = 0;
 mean = 0.4;
 var = 0.005;
 ic_fun = @(x, y) mean + (rand(size(x))*2-1)*var;
-
-
-%ic_fun = @(x, y) 0.1 * cos(2*pi*x) .* cos(2*pi*y);
+ic_fun = @(x, y) 0.1 * cos(2*pi*x) .* cos(2*pi*y);
 
 % reload = load("Square_cahn_hilliard_adaptive_field_5.mat");
 % ic_fun = reload.field;
@@ -87,22 +74,22 @@ ic_fun = @(x, y) mean + (rand(size(x))*2-1)*var;
 % initial_conditions.time = reload.time;
 
 
-initial_conditions.fun_u = ic_fun;
-% initial_conditions.fun_udot = ic_fun_udot;
+problem_data.fun_u = ic_fun;
+% problem_data.fun_udot = ic_fun_udot;
 
 % create folder
-folder_name = strcat('chan_hilliard_results_p',num2str(p),'_nel',num2str(nel),'_lambda',num2str(lambda),'_adaptive_',adaptivity_data.estimator_type);
-status = mkdir(folder_name);
-status = rmdir(folder_name);
+folder_name = strcat('cahn_hilliard_results_p',num2str(p),'_nel',num2str(nel),'_lambda',num2str(lambda),'_adaptive_',adaptivity_data.estimator_type);
+file_name = 'Threepatch_Cahn_Hilliard_adaptive';
 status = mkdir(folder_name);
 
 save_info.folder_name = folder_name;
-save_info.time_save = linspace(-.000001,problem_data.Time_max,51);
+save_info.file_name = file_name;
+save_info.time_save = linspace(-.000001,problem_data.Time_max,4);
 % save_info.time_save = linspace(reload.time-1e-8, problem_data.Time_max, round((Time_max- reload.time)/dt )+1)
 
 % 3) CALL TO THE SOLVER
-[geometry, hmsh, hspace, results] = adaptivity_cahn_hilliard_mp_C1(problem_data, method_data, adaptivity_data, initial_conditions, save_info);
-
+[geometry, hmsh, hspace, results] = ...
+  adaptivity_cahn_hilliard_mp_C1(problem_data, method_data, adaptivity_data, save_info);
 
 %% 4) POST-PROCESSING
 
