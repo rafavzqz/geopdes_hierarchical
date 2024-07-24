f24b76d80cac1b6b6848ccd8fb63a90a29717495
Rafael Vazquez <vazquez@imati.cnr.it>
2024-07-24 11:14:42 +0200

    Added help

--- inst/adaptivity_iga/estimate/adaptivity_estimate_cahn_hilliard.m
+++ inst/adaptivity_iga/estimate/adaptivity_estimate_cahn_hilliard.m
@@ -1,19 +1,57 @@
+% ADAPTIVITY_ESTIMATE_CAHN_HILLIARD: Computation of an indication for Cahn-Hilliard equation, using globally smooth (C^1) hierarchical spaces.
+%
+% USAGE:
+%
+%   est = adaptivity_estimate_cahn_hilliard (u, hmsh, hspace, adaptivity_data)
+%
+% INPUT:
+%
+%   u:       degrees of freedom
+%   hmsh:    object representing the hierarchical mesh (see hierarchical_mesh or hierarchical_mesh_mp)
+%   hspace:  object representing the space of hierarchical splines (see hierarchical_space or hierarchical_space_mp_C1)
+%   adaptivity_data: a structure with the data for the adaptivity method. In particular, it contains the fields:
+%    - estimator_type: either 'field' or 'gradient'
+%
+%
+% OUTPUT:
+%
+%   est: computed a posteriori error indicator
+%
+%  For the details, see the reference paper
+%   C. Bracco, C. Giannelli, A. Reali, M. Torre, R. Vazquez, CMAME 417 (2023), 116355. 
+%
+%
+% Copyright (C) 2023, 2024 Michele Torre, Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function est = adaptivity_estimate_cahn_hilliard (u, hmsh, hspace, adaptivity_data)
 
   % gradient-based estimator ------------------------------------------------
-  if strcmp(adaptivity_data.estimator_type, 'gradient') == 1
+  if (strcmpi(adaptivity_data.estimator_type, 'gradient'))
     normalization = 1;
 
-    % compute the average norm of the gradient 
+    % compute the average norm of the gradient
     integral = integrals_elem (hspace, hmsh, u, 'norm_grad');        
-    est = integral /normalization;
+    est = integral / normalization;
 
   % field-based estimator ---------------------------------------------------
-  elseif strcmp(adaptivity_data.estimator_type, 'field') == 1
+  elseif (strcmpi(adaptivity_data.estimator_type, 'field'))
 
-    % compute the average value of the field       
+    % compute the average value of the field
     integral = integrals_elem (hspace, hmsh, u, 'field');            
-    est = 1-(abs(integral));
+    est = 1 - (abs(integral));
   end
 end
 
@@ -36,9 +74,9 @@ function integral = integrals_elem (hspace, hmsh, u, type)
     if (hmsh.nel_per_level(ilev) > 0)
       msh_level = hmsh.msh_lev{ilev};
 
-      if strcmp(type, 'field') == 1
+      if (strcmpi(type, 'field'))
         sp_level = sp_evaluate_element_list (hspace.space_of_level(ilev), hmsh.msh_lev{ilev}, 'value', true);
-      elseif strcmp(type, 'norm_grad') == 1
+      elseif (strcmpi(type, 'norm_grad'))
         sp_level = sp_evaluate_element_list (hspace.space_of_level(ilev), hmsh.msh_lev{ilev}, 'value', false, 'gradient', true);
       end
 
@@ -57,13 +95,13 @@ function integral = integrals_elem_level (sp, msh, u, type)
   w = msh.quad_weights .* msh.jacdet;
   elem_size = sum (ones(size(w)) .* w);
 
-  if (strcmp(type, 'field') == 1)
-    eu = sp_eval_msh (u, sp, msh,'value');
+  if (strcmpi(type, 'field'))
+    eu = sp_eval_msh (u, sp, msh, 'value');
     valu = reshape (eu, msh.nqn, msh.nel);
     integral = (sum (valu .* w)) ./ elem_size;
 
-  elseif (strcmp(type, 'norm_grad') == 1)
-    eu = sp_eval_msh (u, sp, msh,'gradient');
+  elseif (strcmpi(type, 'norm_grad'))
+    eu = sp_eval_msh (u, sp, msh, 'gradient');
     grad_valu = reshape (eu, sp.ncomp, msh.rdim, msh.nqn, msh.nel);
     integral = sum (reshape ( sqrt (sum ((grad_valu).^2, 2) ), [msh.nqn, msh.nel]) .* w);
     integral =  integral ./ elem_size;
